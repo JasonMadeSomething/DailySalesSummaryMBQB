fea7dd19ed806d745e3d211b1b088c2029668e8a
Jason Teitelman <jasonmadesomething@gmail.com>
2023-07-04 09:43:10 -0400

    Cleaned up some stuff

--- DailySalesSummary/Program.cs
+++ DailySalesSummary/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Metrics;
 
 public class Program
 {
@@ -13,5 +18,25 @@ public class Program
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
+            })
+            .ConfigureServices((IServiceCollection services) =>
+            {
+                services.AddOpenTelemetry()
+                    .WithTracing(builder => builder
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddJaegerExporter(options =>
+                        {
+                            options.AgentHost = "localhost";
+                            options.AgentPort = 6831;
+                        })
+                        .AddConsoleExporter()
+                        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("DailySalesSummary"))
+                    )
+                    .WithMetrics(builder => builder
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddConsoleExporter()
+                    );
             });
 }
